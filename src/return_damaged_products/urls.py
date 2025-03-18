b49d0d2f33b0199d2e40f5bed176d3ca0d643938
meenatchi-abirami <abirami2349devaraj@gmail.com>
2025-03-18 15:27:37 +0530

    code updated

--- src/return_damaged_products/urls.py
+++ src/return_damaged_products/urls.py
@@ -4,6 +4,6 @@ router = APIRouter(prefix="/return_damage_product")
 
 router.add_api_route("/get_damaged_return_categories", get_damaged_return_categories, methods=["GET"])
 
-router.add_api_route("/return_damaged_product", return_damaged_product, methods=["POST"])
+# router.add_api_route("/return_damaged_product", return_damaged_product, methods=["POST"])
 
 
